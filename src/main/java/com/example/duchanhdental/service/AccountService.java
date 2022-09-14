41949cf31a83644d8588fdd37f5b3abcebc694c3
nguyenkhoi2202 <63914061+nguyenkhoi2202@users.noreply.github.com>
2022-09-14 09:09:29 +0700

    Delete src directory

--- src/main/java/com/example/duchanhdental/service/AccountService.java
+++
@@ -1,4 +0,0 @@
-package com.example.duchanhdental.service;
-
-public interface AccountService {
-}
--- src/main/java/com/example/duchanhdental/service/impl/AccountServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.example.duchanhdental.service.impl;
-
-import com.example.duchanhdental.service.AccountService;
-
-public class AccountServiceImpl implements AccountService {
-}
